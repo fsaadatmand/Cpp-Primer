fc54791e3128249496e90608f59a89e9fc6f61f7
cdude <cdude996@gmail.com>
2019-04-02 16:33:35 -0400

    fix typo

--- chapter02/2.18.cpp
+++ chapter02/2.18.cpp
@@ -1,7 +1,7 @@
 /*
  * Exercise 2.18: Write code to change the value of a pointer. Write code to
  * change the value to which the pointer points.
- * By Faisal Ssadatmand
+ * By Faisal Saadatmand
  */
 
 #include <iostream>
