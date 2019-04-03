2c0699025def166d48dacc3c3e0a1d0326f5ed24
cdude <cdude996@gmail.com>
2019-04-02 22:43:04 -0400

    add question

--- chapter01/1.20.cpp
+++ chapter01/1.20.cpp
@@ -1,3 +1,11 @@
+/*
+ * Exercise 1.20: http://www.informit.com/title/0321714113 contains a copy of
+ * Sales_item.h in the Chapter 1 code directory. Copy that file to your working
+ * directory. Use it to write a program that reads a set of book sales
+ * transactions, writing each transaction to the standard output.
+ * By Faisal Saadatmand
+*/
+
 #include <iostream>
 #include "Sales_item.h"
 
