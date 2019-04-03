d9bcd4a1cab12bc0a35cd61f5c394542f9de5e90
cdude <cdude996@gmail.com>
2019-04-02 22:46:04 -0400

    rewrite answer

--- chapter01/1.23.cpp
+++ chapter01/1.23.cpp
@@ -10,28 +10,22 @@ by Faisal Saadatmand
 
 int main()
 {
-	int isbn1, isbn2, isbn3;
-	Sales_item item1, item2, item3;
+	Sales_item currItem, item;
 
-	isbn1 = isbn2 = isbn3 = 0;
-	std::cout << "Enter 3 transactions:" << std::endl;
-	std::cin >> item1 >> item2 >> item3;
-	isbn1++;
-	if (item1.isbn() == item2.isbn())
-		isbn1++;
-	else
-		isbn2++;
-	if (item1.isbn() == item3.isbn())
-		isbn1++;
-	else if (item2.isbn() == item3.isbn())
-		isbn2++;
-	else
-		isbn3++;
-	std::cout << "ISBN: "  << item1.isbn() << "# trans: "
-		<< isbn1 << std::endl;
-	std::cout << "ISBN: "  << item2.isbn() << "# trans: "
-		<< isbn2 << std::endl;
-	std::cout << "ISBN: "  << item3.isbn() << "# trans: "
-		<< isbn3 << std::endl;
+	std::cout << "Enter transactions:" << std::endl;
+	if (!(std::cin >> currItem)) {
+		std::cerr << "Ivalid data" << std::endl;;
+		return -1;
+	}
+	int count = 1;
+	while (std::cin >> item)
+		if (currItem.isbn() == item.isbn())
+			count++;
+		else {
+			std::cout << currItem.isbn() << " " << count <<  std::endl;
+			currItem = item;
+			count = 1;
+		}
+	std::cout << currItem.isbn() << " " << count << std::endl;
 	return 0;
 }
