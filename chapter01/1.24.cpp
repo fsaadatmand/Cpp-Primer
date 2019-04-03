de3ffc4e245984c58d493da81bf3bdc5dc790362
cdude <cdude996@gmail.com>
2019-04-02 22:46:37 -0400

    rewrite answer

--- chapter01/1.24.cpp
+++ chapter01/1.24.cpp
@@ -2,47 +2,30 @@
  * Exercise 1.24: Test the previous program by giving multiple transactions
  * representing multiple ISBNs. The records for each ISBN should be grouped
  * together.
- *
  * by Faisal Saadatmand
- */
+*/
 
 #include <iostream>
 #include "Sales_item.h"
 
 int main()
 {
-	int count1, count2, count3;
-	Sales_item item1, item2, item3, isbnGroup1, isbnGroup2, isbnGroup3;
+	Sales_item currItem, item;
 
-	std::cout << "Enter 3 transactions:" << std::endl;
-	std::cin >> item1 >> item2 >> item3;
-	count1 = count2 = count3 = 0;
-	isbnGroup1 = item1;
-	count1++;
-	if (item2.isbn() == isbnGroup1.isbn()) {
-		isbnGroup1 += item2;
-		count1++;
-	} else {
-		isbnGroup2 = item2;
-		count2++;
+	std::cout << "Enter transactions:" << std::endl;
+	if (!(std::cin >> currItem)) {
+		std::cerr << "Ivalid data" << std::endl;;
+		return -1;
 	}
-
-	if (item3.isbn() == isbnGroup1.isbn()) {
-		isbnGroup1 += item3;
-		count1++;
-	} else if (item3.isbn() == isbnGroup2.isbn()) {
-		isbnGroup2 += item3;
-		count2++;
-	} else {
-		isbnGroup3 = item3;
-		count3++;
-	}
-
-	if (count1)
-		std::cout << isbnGroup1 << std::endl;
-	if (count2)
-		std::cout << isbnGroup2 << std::endl;
-	if (count3)
-		std::cout << isbnGroup3 << std::endl;
+	int count = 1;
+	while (std::cin >> item)
+		if (currItem.isbn() == item.isbn())
+			count++;
+		else {
+			std::cout << currItem.isbn() << " " << count << std::endl;
+			currItem = item;
+			count = 1;
+		}
+	std::cout << currItem.isbn() << " " << count << std::endl;
 	return 0;
 }
