5b8340f6858019046b3979470ac7055767dc4bf4
cdude <cdude996@gmail.com>
2019-04-05 00:59:43 -0400

    initial commit

---
+++ chapter02/2.39.cpp
@@ -0,0 +1,14 @@
+/*
+ * Exercise 2.39: Compile the following program to see what happens when you
+ * forget the semicolon after a class definition. Remember the message for
+ * future reference.
+ *
+ *  By Faisal Saadatmand
+ */
+
+struct Foo { /* empty */ } // Note: no semicolon
+
+int main()
+{
+	return 0;
+}
