cd47d188d7cee1de8acd9846d1123c88ebac42e2
cdude <cdude996@gmail.com>
2019-04-12 15:15:40 -0400

    fix loop condition to test for valid input

--- chapter04/4.10.cpp
+++ chapter04/4.10.cpp
@@ -2,8 +2,6 @@
  * Exercise 4.10: Write the condition for a while loop that would read ints
  * from the standard input and stop when the value read is equal to 42.
  *
- * Note: the loop does not test for valid input.
- *
  * By Faisal Saadatmand
  */
 
@@ -13,10 +11,8 @@ int main()
 {
 	long int x;
 
-	std::cin >> x;
-	while (x != 42) {
-		std::cin >> x;
+	while ((std::cin >> x) && x != 42)
 		std::cout << x << '\n';
-	}
+
 	return 0;
 }
