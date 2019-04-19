4f03632f20d75653a419aba30b33e7d01682b34e
cdude <cdude996@gmail.com>
2019-04-19 16:57:09 -0400

    add encapsulation

--- chapter07/Person.h
+++ chapter07/Person.h
@@ -3,11 +3,11 @@
 
 #include <string>
 
-struct Person;
-std::istream& read(std::istream &, Person &);
-std::ostream& print(std::ostream &, const Person &);
-
-struct Person {
+class Person {
+	// friends for non-members
+	friend std::istream& read(std::istream &, Person &);
+	friend std::ostream& print(std::ostream &, const Person &);
+	public:
 	// constructors
 	Person() = default;
 	Person(std::string &n, std::string &a) : pName(n), pAddress(a) {} 
@@ -15,9 +15,14 @@ struct Person {
 	/// member functions
 	std::string name() const { return pName; }
 	std::string address() const { return pAddress; }
+	private:
 	//members
 	std::string pName;
 	std::string pAddress;
 };
+
+// non-memebrs
+std::istream& read(std::istream &, Person &);
+std::ostream& print(std::ostream &, const Person &);
 
 #endif
