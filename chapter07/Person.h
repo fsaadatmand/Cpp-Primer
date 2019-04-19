04feb4a2cf8ffc0a48a3a4991c1310f9fcb27b94
cdude <cdude996@gmail.com>
2019-04-19 14:55:48 -0400

    add constructors

--- chapter07/Person.h
+++ chapter07/Person.h
@@ -1,26 +1,23 @@
-/*
- * Exercise 7.5: Provide operations in your Person class to return the name and
- * address. Should these functions be const? Explain your choice.
- * 
- * Answer: yes, since these operations do not modify the members of the class
- * they are returning.
- *
- * By Faisal Saadatmand
- */
-
 #ifndef PERSON_H
 #define PERSON_H
 
 #include <string>
 
+struct Person;
+std::istream& read(std::istream &, Person &);
+std::ostream& print(std::ostream &, const Person &);
+
 struct Person {
+	// constructors
+	Person() = default;
+	Person(std::string &n, std::string &a) : pName(n), pAddress(a) {} 
+	Person(std::istream &is) { read(is, *this); }
+	/// member functions
 	std::string name() const { return pName; }
 	std::string address() const { return pAddress; }
+	//members
 	std::string pName;
 	std::string pAddress;
 };
-
-std::istream& read(std::istream &, Person &);
-std::ostream& print(std::ostream &, const Person &);
 
 #endif
