d44bbdb94e38374b0b26d57cac6e0c4c38d82570
cdude <cdude996@gmail.com>
2019-04-19 16:57:25 -0400

    initial commit

---
+++ chapter07/7.21.h
@@ -0,0 +1,58 @@
+/*
+ * Exercise 7.21: Update your Sales_data class to hide its implementation. The
+ * pro- grams you’ve written to use Sales_data operations should still continue
+ * to work.  Recompile those programs with your new class definition to verify
+ * that they still work.
+ *
+ * By Faisal Saadatmand
+*/
+
+#ifndef SALES_DATA_H
+#define SALES_DATA_H
+
+#include <string>
+
+class Sales_data {
+	// friend dcl for non-members
+	friend std::istream& read(std::istream &, Sales_data &);
+	friend std::ostream& print(std::ostream &, const Sales_data &);
+	friend Sales_data add(Sales_data &, Sales_data &);
+	public:
+	// constructors
+	Sales_data() = default;
+	Sales_data(const std::string &s) : bookNo(s) { }
+	Sales_data(const std::string &s, unsigned n, double p) :
+		       bookNo(s), units_sold(n), revenue(p * n) { } 
+	Sales_data(std::istream &is) { read(is, *this); }
+	// members
+	std::string isbn() const { return bookNo;}
+	Sales_data& combine(const Sales_data &);
+	private:
+	double avg_price() const;
+	std::string bookNo;
+	unsigned units_sold = 0;
+	double revenue = 0.0;
+};
+
+// member function's definitions
+inline Sales_data& Sales_data::combine(const Sales_data &rhs)
+{
+	units_sold += rhs.units_sold; 
+	revenue += rhs.revenue;
+	return *this;
+}
+
+inline double Sales_data::avg_price() const
+{
+	if (units_sold)
+		return revenue / units_sold;
+	else
+		return 0;
+}
+
+// non-member function's declarations
+std::istream& read(std::istream &, Sales_data &);
+std::ostream& print(std::ostream &, const Sales_data &);
+Sales_data add(Sales_data &, Sales_data &);
+
+#endif
---
+++ chapter07/7.22.h
@@ -0,0 +1,34 @@
+/*
+ * Exercise 7.22: Update your Person class to hide its implementation. 
+ *
+ * By Faisal Saadatmand
+ */
+
+#ifndef PERSON_H
+#define PERSON_H
+
+#include <string>
+
+class Person {
+	// friends for non-members
+	friend std::istream& read(std::istream &, Person &);
+	friend std::ostream& print(std::ostream &, const Person &);
+	public:
+	// constructors
+	Person() = default;
+	Person(std::string &n, std::string &a) : pName(n), pAddress(a) {} 
+	Person(std::istream &is) { read(is, *this); }
+	/// member functions
+	std::string name() const { return pName; }
+	std::string address() const { return pAddress; }
+	private:
+	//members
+	std::string pName;
+	std::string pAddress;
+};
+
+// non-memebrs
+std::istream& read(std::istream &, Person &);
+std::ostream& print(std::ostream &, const Person &);
+
+#endif
