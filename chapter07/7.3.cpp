e5b6f40ce08f244620d3d0a8fe7d37be43fee39a
cdude <cdude996@gmail.com>
2019-04-19 12:38:54 -0400

    fix revenue calculations

--- chapter07/7.3.cpp
+++ chapter07/7.3.cpp
@@ -11,19 +11,23 @@
 int main()
 {
 	Sales_data total;
+	double price = 0.0;
 
 	if (std::cin >> total.bookNo && std::cin
-			     >> total.units_sold && std::cin >> total.revenue) {
+			     >> total.units_sold && std::cin >> price) {
+		total.revenue = total.units_sold * price;
 		Sales_data trans;
 			while (std::cin >> trans.bookNo && std::cin
-						 >> trans.units_sold && std::cin >> trans.revenue) {
-				if (total.isbn() == trans.isbn())
+						 >> trans.units_sold && std::cin >> price) {
+				trans.revenue = trans.units_sold * price;
+				if (total.isbn() == trans.isbn()) {
 					total.combine(trans);
-				else {
+				} else {
 					std::cout << total.isbn() << " "
 						      << total.units_sold << " "
 							  << total.revenue << " "
 							  << total.avg_price() << std::endl;
+					total = trans;
 				}
 			}
 			std::cout << total.isbn() << " "
