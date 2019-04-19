1ed0c87bb0c945eb848be7df2dcce5dd801c53e6
cdude <cdude996@gmail.com>
2019-04-19 11:13:49 -0400

    add explicit inline to member functions defined outside class

--- chapter07/7.2.h
+++ chapter07/7.2.h
@@ -19,14 +19,14 @@ struct Sales_data {
 	double revenue = 0.0;
 };
 
-Sales_data& Sales_data::combine(const Sales_data &rhs)
+inline Sales_data& Sales_data::combine(const Sales_data &rhs)
 {
 	units_sold += rhs.units_sold; 
 	revenue += rhs.revenue;
 	return *this;
 }
 
-double Sales_data::avg_price() const
+inline double Sales_data::avg_price() const
 {
 	if (units_sold)
 		return revenue / units_sold;
