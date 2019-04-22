8f3fe3a94eab34373d00199e6037adc693bd9b2c
cdude <cdude996@gmail.com>
2019-04-22 18:19:22 -0400

    improve note

--- chapter07/7.12.h
+++ chapter07/7.12.h
@@ -2,9 +2,11 @@
  * Exercise 7.12: Move the definition of the Sales_data constructor that takes
  * an istream into the body of the Sales_data class.
  *
- * Note: read is not a member function, as such, it cannot be included in the
- * class' body without a previous declaration. I moved up the non-member
- * declarations to the top of the header file.
+ * Note: read is not a member function. As such, it cannot be included in the
+ * class' body without a prior declaration. Thus, we have include its
+ * declarations before its use in the constructor. To achieve this, and at the
+ * same time not break read dependency on Sales_class, we must use a forward
+ * declaration. See page 278 -279.
  *
  * By Faisal Saadatmand
  */
@@ -14,8 +16,8 @@
 
 #include <string>
 
+struct Sales_data;                // class forward declaration
 // non-member function's declarations
-struct Sales_data;
 std::istream& read(std::istream &, Sales_data &);
 std::ostream& print(std::ostream &, const Sales_data &);
 Sales_data add(Sales_data &, Sales_data &);
