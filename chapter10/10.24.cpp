868fe2a84bf906e0f16cd2ea1250b1e8ee4fb624
cdude <cdude996@gmail.com>
2019-05-08 10:43:29 -0400

    add boundary check and using placeholders

--- chapter10/10.24.cpp
+++ chapter10/10.24.cpp
@@ -12,23 +12,25 @@
 #include <algorithm>
 #include <functional>
 
-bool check_size(const std::string &s, const std::string::size_type &sz)
+bool check_size(const std::string &s, const std::string::size_type sz)
 {
 	return s.size() >= sz;
 }
 
 int main()
 {
+	using std::placeholders::_1; using std::placeholders::_2;
 	std::vector<int> ivec{1, 4, 0, 7, 11, 25, 3, 9};
 	// using find_if_not, instead of find_if: see note below.
 	auto elem = find_if_not(ivec.begin(), ivec.end(),
-			bind(check_size, "greater than me!", std::placeholders::_1));
-	std::cout << *elem << '\n';
+			bind(check_size, "greater than me!", _1));
+	(elem != ivec.end()) ? std::cout << *elem << '\n' 
+		                 : std::cout << ("No match found") << '\n';
 	// NOTE: inverting the parameters of check_size with bind does not seem to
 	// have an effect on the expression inside the function--it is still
 	// evaluated as s.size() >= sz. I suspect it has to do with the fact that
 	// expression is between s.size (not s) and sz:
-	auto invert = bind(check_size, std::placeholders::_2, std::placeholders::_1);
+	auto invert = bind(check_size, _2, _1);
 	std::cout << invert(20, "try me") << '\n';
 	return 0;
 }
