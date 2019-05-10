ed49aa6e0ae605102619d3003d0cc777110202b7
cdude <cdude996@gmail.com>
2019-05-10 12:05:55 -0400

    use endl to flush bufferbefore exit

--- chapter10/10.34.cpp
+++ chapter10/10.34.cpp
@@ -14,6 +14,6 @@ int main()
 	std::vector<int> ivec{0, 1, 2, 3, 4, 5, 6, 7, 8 , 9};
 	std::ostream_iterator<int> out_iter(std::cout, " ");
 	copy(ivec.crbegin(), ivec.crend(), out_iter);
-	std::cout << '\n';
+	std::cout << std::endl;
 	return 0;
 }
