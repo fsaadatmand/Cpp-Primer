e533a80291011b17fd59318230311ed073fa95ce
cdude <cdude996@gmail.com>
2019-05-14 11:42:22 -0400

    add comment

--- chapter11/11.12.cpp
+++ chapter11/11.12.cpp
@@ -17,7 +17,7 @@ int main()
 	int number;
 
 	while (std::cin >> word && std::cin >> number)
-		strInt.push_back({word, number});
+		strInt.push_back({word, number});  // push list initialized pairs
 
 	for (const auto &pair : strInt)
 		std::cout << pair.first << ' '
