62e3880ff6ca39efab0aa3a96e828cb1a006ce4c
cdude <cdude996@gmail.com>
2019-05-26 01:36:11 -0400

    rename variables

--- chapter12/12.24.cpp
+++ chapter12/12.24.cpp
@@ -9,21 +9,21 @@
  * In the context of this program, a string is a set made of non-whitespace
  * characters that are contiguous.
  *
- * arr is a pointer to the dynamically allocated array. arrLen is the number of
+ * str is a pointer to the dynamically allocated array. length is the number of
  * elements in the array and stepSize is a const by which we can grow the array
  * length. i is a counter that has a twofold usage: to check the number of
  * characters read so far and to hold the current position in array.
  *
  * The program reads a character at a time (including whitespaces) from the
- * standard input. If there is enough memory in the arr, it writes the
+ * standard input. If there is enough memory in the str, it writes the
  * currently read character into it and increments i with the postfix operator,
- * otherwise it calls charArrayRealloc to (re)allocate memory. Note: arrLen
+ * otherwise it calls charArrayRealloc to (re)allocate memory. Note: length
  * starts at 0, which satisfies the if statement's condition at the first
- * iteration. Finally, the program prints arr to the standard output.
+ * iteration. Finally, the program prints str to the standard output.
  *
- * Inside the if statement, arrLen is incremented by stepSize. The function
- * charArrayRealloc is called with arr, the old length and the new length of
- * the arr. Inside charArrayRealloc, we check if this is the first time the
+ * Inside the if statement, length is incremented by stepSize. The function
+ * charArrayRealloc is called with str, the old length and the new length of
+ * the str. Inside charArrayRealloc, we check if this is the first time the
  * function is called. In which case, a new block is allocated and a pointer to
  * it is returned, otherwise the memory is reallocated using the passed
  * arguments and a pointer to the new memory location is returned; the old
@@ -56,23 +56,23 @@ char *charArrayRealloc(char *p, const size_t &oldSize, const size_t &newSize)
 // read a string into a dynamically allocated array.
 int main()
 {
-	char ch, *arr = nullptr;
+	char ch, *str = nullptr;
 	constexpr size_t stepSize = 25;
-	size_t arrLen = 0, i = 0;
+	size_t length = 0, i = 0;
 	while (std::cin >> ch >> std::noskipws && !isspace(ch)) {
-		if (i == arrLen) {       // need more memory
-			arrLen += stepSize;  // grow the size of the array
-			arr = charArrayRealloc(arr, i, arrLen);
-			if (!arr) {
+		if (i == length) {       // need more memory
+			length += stepSize;  // grow the size of the array
+			str = charArrayRealloc(str, i, length);
+			if (!str) {
 				std::cerr << "Could not allocate memory\n";
 				return -1;
 			}
 		}
-		arr[i++] = ch;
+		str[i++] = ch;
 	}
-	arr[i] = '\0';
-	std::cout << std::string(arr) << std::endl;
-	delete [] arr;
-	arr = nullptr;               // good practice
+	str[i] = '\0';
+	std::cout << std::string(str) << std::endl;
+	delete [] str;
+	str = nullptr;               // good practice
 	return 0;
 }
