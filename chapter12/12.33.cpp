bf453214c1ea16b40be1f0d2c398c514cd62fdab
cdude <cdude996@gmail.com>
2019-06-04 22:29:55 -0400

    fix typo

--- chapter12/12.33.cpp
+++ chapter12/12.33.cpp
@@ -1,5 +1,5 @@
 /*
- * Exercise 12.33: In Chapter 15 we’ll extend our query system and will need
+ * Exercise 12.33: In Chapter 15 we'll extend our query system and will need
  * some additional members in the QueryResult class. Add members named begin
  * and end that return iterators into the set of line numbers returned by a
  * given query, and a member named get_file that returns a shared_ptr to the
