0519274b6046f2218302a7803414568c44f7fab3
cdude <cdude996@gmail.com>
2019-06-07 16:01:36 -0400

    initial commit

---
+++ chapter14/14.16b.h
@@ -0,0 +1,179 @@
+/*
+ * Exercise 14.16: Define equality and inequality operators for your StrBlob (§
+ * 12.1.1, p. 456), StrBlobPtr (§ 12.1.6, p. 474), StrVec (§ 13.5, p. 526), and
+ * String (§ 13.5, p. 531) classes.
+ *
+ * By Faisal Saadatmand
+ */
+
+#ifndef STR_VEC_H
+#define STR_VEC_H
+
+#include <initializer_list>
+#include <iostream>
+#include <memory>
+#include <string>
+#include <utility>
+
+class StrVec {
+	friend bool operator==(const StrVec &, const StrVec &);
+	friend bool operator!=(const StrVec &, const StrVec &);
+	public:
+		StrVec() : elements(nullptr), first_free(nullptr), cap(nullptr) { }
+		StrVec(std::initializer_list<std::string>);
+		StrVec(const StrVec &);
+		StrVec(StrVec &&) noexcept;  // move constructor
+		StrVec& operator=(const StrVec &);
+		StrVec& operator=(StrVec &&) noexcept; // move assignment operator
+		~StrVec() { free(); }
+		void push_back(const std::string &);
+		size_t size() const { return first_free - elements; }
+		size_t capacity() const { return cap - elements; }
+		std::string *begin() const { return elements; }
+		std::string *end() const { return first_free; }
+		void reserve(const size_t &);
+		void resize(const size_t &, const std::string &s = "");
+	private:
+		static std::allocator<std::string> alloc;
+		void chk_n_alloc() { if (size() == capacity()) reallocate(); }
+		std::pair<std::string *, std::string *>
+			alloc_n_copy(const std::string *, const std::string *);
+		void free();
+		void reallocate(size_t n = 0);
+		std::string *elements;
+		std::string *first_free;
+		std::string *cap;
+};
+
+// static member
+std::allocator<std::string> StrVec::alloc;
+
+// constructor
+StrVec::StrVec(std::initializer_list<std::string> il) : StrVec()
+{
+	reallocate(il.size());
+	for (auto p = il.begin(); p != il.end(); ++p)
+		alloc.construct(first_free++, *p);
+}
+
+// copy-control functions
+StrVec::StrVec(const StrVec &s)
+{
+	auto newdata = alloc_n_copy(s.begin(), s.end());
+	elements = newdata.first;
+	first_free = cap = newdata.second;
+	std::cout << "StrVec(const StrVec &)\n";
+}
+
+StrVec::StrVec(StrVec &&s) noexcept : elements(s.elements),
+	first_free(s.first_free), cap(s.cap)
+{
+	s.elements = s.first_free = cap = nullptr;
+	std::cout << "StrVec(StrVec &&)\n";
+}
+
+StrVec&
+StrVec::operator=(const StrVec &rhs)
+{
+	auto data = alloc_n_copy(rhs.begin(), rhs.end());
+	free();
+	elements = data.first;
+	first_free = cap = data.second;
+	std::cout << "StrVec& operator=(const StrVec &)\n";
+	return *this;
+}
+
+StrVec&
+StrVec::operator=(StrVec &&rhs) noexcept
+{
+	if (this != &rhs) {
+		free();
+		elements = rhs.elements;
+		first_free = rhs.first_free;
+		cap = rhs.cap;
+		rhs.elements = rhs.first_free = rhs.cap = nullptr;
+	}
+	std::cout << "StrVec& operator=(StrVec &&)\n";
+	return *this;
+}
+
+// member functions
+void
+StrVec::push_back(const std::string &s)
+{
+	chk_n_alloc();
+	alloc.construct(first_free++, s);
+}
+
+std::pair<std::string *, std::string *>
+StrVec::alloc_n_copy(const std::string *b, const std::string *e)
+{
+	auto data = alloc.allocate(e - b);
+	return {data, uninitialized_copy(b, e, data)};
+}
+
+void
+StrVec::free()
+{
+	if (elements) {
+		for (auto p = first_free; p != elements;)
+			alloc.destroy(--p);
+		alloc.deallocate(elements, cap - elements);
+	}
+}
+
+void
+StrVec::reallocate(size_t n) // n is optional
+{
+	auto newcapacity = (n) ? n : (size()) ? 2 * size() : 1;
+	auto newdata = alloc.allocate(newcapacity);
+	auto dest = newdata;  // points to the next free pos in the new array
+	auto elem = elements; // points to next element in the old array
+	for (size_t i = 0; i != size(); ++i)
+		alloc.construct(dest++, std::move(*elem++));
+	free();
+	elements = newdata;
+	first_free = dest;
+	cap = elements + newcapacity;
+}
+
+void
+StrVec::reserve(const size_t &n)
+{
+	if (n > capacity()) // requested size is bigger than capacity
+		reallocate(n);  // use optional argument
+}
+
+void
+StrVec::resize(const size_t &n, const std::string &s)
+{
+	if (n > capacity())
+		reallocate(n);
+	auto newsize = first_free + (n - size());	// points to the new first_free
+	while (first_free != newsize)
+		if (first_free < newsize)
+			alloc.construct(first_free++, s);
+		else
+			alloc.destroy(--first_free);
+}
+// non-member functions
+bool operator==(const StrVec &lhs, const StrVec &rhs)
+{
+	bool result = false;
+	if (lhs.size() == rhs.size()) {
+		result = true;
+		auto rhsP = rhs.elements;
+		for (const auto &elem : lhs)
+			if (elem != *rhsP++) {
+				result = false;
+				break;
+			}
+	}
+	return result;
+}
+
+bool operator!=(const StrVec &lhs, const StrVec &rhs)
+{
+	return !(lhs == rhs);
+}
+#endif
---
+++ chapter14/14.16c.h
@@ -0,0 +1,204 @@
+/* 
+ * Exercise 14.16: Define equality and inequality operators for your StrBlob (§
+ * 12.1.1, p. 456), StrBlobPtr (§ 12.1.6, p. 474), StrVec (§ 13.5, p. 526), and
+ * String (§ 13.5, p. 531) classes.
+ *
+ * By Faisal Saadatmand
+ *
+ */
+
+#ifndef MY_STRING_H
+#define MY_STRING_H
+
+#include <iostream>
+#include <memory>
+#include <cstring>
+
+class String {
+	friend std::istream& operator>>(std::istream &, String &);
+	friend std::ostream& operator<<(std::ostream &, const String &);
+	friend bool operator==(const String &, const String &);
+	friend bool operator!=(const String &, const String &);
+	public:
+		String() = default;
+		String(const char *);
+		String(const String &);
+		String(String &&) noexcept;
+		String& operator=(const String &);
+		String& operator=(String &&) noexcept;
+		~String() { free(); }
+		void push_back(const char &);
+		void push_back(char &&);
+		void pop_back();
+		size_t size() const { return first_free - elements; }
+		size_t capacity() const { return cap - elements; }
+		char *begin() const { return elements; }
+		char *end() const { return first_free; }
+		void reserve(const size_t &n) { if (n > capacity()) reallocate(n); }
+		void resize(const size_t &, const char &ch = '\0');
+	private:
+		static std::allocator<char> alloc;
+		void chk_n_alloc() { if (size() == capacity()) reallocate(); }
+		std::pair<char *, char *> alloc_n_copy(const char *, const char *);
+		void reallocate(const size_t &n = 0);
+		void free();
+		char *elements{nullptr};
+		char *first_free{nullptr};
+		char *cap{nullptr};
+};
+
+// static members
+std::allocator<char> String::alloc;
+
+// constructors
+String::String(const char *s)
+{
+	auto data = alloc_n_copy(s, s + std::strlen(s));
+	elements = data.first; 
+	first_free = cap = data.second;
+}
+
+// copy-controls functions
+String::String(const String &s)
+{
+	auto newdata = alloc_n_copy(s.begin(), s.end());
+	elements = newdata.first;
+	first_free = cap = newdata.second;
+	std::cout << "String(const String &)\n";
+}
+
+String::String(String &&s) noexcept : elements(s.elements),
+	first_free(s.first_free), cap(s.cap)
+{
+	s.elements = s.first_free = cap = nullptr;
+	std::cout << "String(String &&rhs)\n";
+}
+
+String&
+String::operator=(const String &rhs)
+{
+	auto newdata = alloc_n_copy(rhs.begin(), rhs.end());
+	free();
+	elements = newdata.first;
+	first_free = cap = newdata.second;
+	return *this;
+	std::cout << "String& operator=(const String &)\n";
+}
+
+String&
+String::operator=(String &&rhs) noexcept
+{
+	if (this != &rhs) {
+		free();
+		elements = rhs.elements;
+		first_free = rhs.first_free;
+		cap = rhs.cap;
+		elements = first_free = cap = nullptr;
+	}
+	std::cout << "String& operator=(String &&rhs)\n";
+	return *this;
+}
+
+// member functions
+void
+String::push_back(const char &ch)
+{
+	chk_n_alloc();
+	alloc.construct(first_free++, ch);
+}
+
+void
+String::push_back(char &&ch)
+{
+	chk_n_alloc();
+	alloc.construct(first_free++, std::move(ch));
+}
+
+void
+String::pop_back()
+{
+	if (first_free == elements)
+		throw std::out_of_range("pop on empty String");
+	alloc.destroy(--first_free);
+}
+
+std::pair<char *, char *>
+String::alloc_n_copy(const char *b, const char *e)
+{
+	auto data = alloc.allocate(e - b);
+	return {data, std::uninitialized_copy(b, e, data)};
+}
+
+void
+String::reallocate(const size_t &n)
+{
+	auto newcapacity = (n) ? n : (size()) ? 2 * size() : 1;
+	auto newdata = alloc.allocate(newcapacity);
+	auto dest = newdata;
+	auto elem = elements;
+	for (size_t i = 0; i != size(); ++i)
+		alloc.construct(dest++, std::move(*elem++));
+	free();
+	elements = newdata;
+	first_free = dest;
+	cap = elements + newcapacity;
+}
+
+void
+String::free()
+{
+	for (auto &p = first_free; p != elements;)
+		alloc.destroy(--p);
+	alloc.deallocate(elements, capacity());
+}
+
+void
+String::resize(const size_t &n, const char &ch)
+{
+	reserve(n);
+	auto newsize = first_free + (n - size());
+	while (first_free != newsize)
+		if (first_free < newsize)
+			alloc.construct(first_free++, ch);
+		else
+			alloc.destroy(--first_free);
+}
+
+// non-member functions
+std::istream& operator>>(std::istream &is, String &s)
+{
+	std::string input;
+	is >> input;
+	if (is)
+		for (const auto &ch : input)
+			s.push_back(std::move(ch));
+	return is;
+}
+
+std::ostream& operator<<(std::ostream &os, const String &s)
+{
+	for (const auto &ch : s)
+		os << ch; 
+	return os;
+}
+
+bool operator==(const String &lhs, const String &rhs)
+{
+	bool result = false;
+	if (lhs.size() == rhs.size()) {
+		result = true;
+		auto rhsP = rhs.begin();
+		for (const auto &ch : lhs)
+			if (ch != *rhsP++) {
+				result = false;
+				break;
+			}
+	}
+	return result;
+}
+
+bool operator!=(const String &lhs, const String &rhs)
+{
+	return !(lhs == rhs);
+}
+#endif
