d414cd000fce1c50e6bc73fcc12aba42add35519
cdude <cdude996@gmail.com>
2019-06-06 22:18:50 -0400

    initial commit

---
+++ chapter13/13.58.cpp
@@ -0,0 +1,84 @@
+/*
+ * Exercise 13.58: Write versions of class Foo with print statements in their
+ * sorted functions to test your answers to the previous two exercises.
+ *
+ * By Faisal Saadatmand
+ *
+ * Note: uncomment the version of sorted() const & to test the code.
+ */
+
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+class Foo {
+	public:
+		Foo() = default;
+		Foo(const std::vector<int> &v) : data(v) { }
+		Foo(const Foo &rhs) : data(rhs.data) { }
+		Foo& operator=(const Foo &) &;
+		Foo sorted() &&;
+		Foo sorted() const &;
+		auto begin() const { return data.begin(); }
+		auto end() const { return data.end(); }
+	public:
+		std::vector<int> data;
+};
+
+Foo&
+Foo::operator=(const Foo &rhs) &
+{
+	auto newdata = rhs.data;
+	data = newdata;
+	return *this;
+}
+
+
+//Foo
+//Foo::sorted() const & {
+//	std::cout << "sorted() const &\n";
+//	Foo ret(*this);
+//	return ret.sorted();
+//}
+
+
+Foo Foo::sorted() const &
+{
+	std::cout << "sorted() const &\n";
+	return Foo(*this).sorted();
+}
+
+Foo
+Foo::sorted() &&
+{
+	std::cout << "sorted() &&\n";
+	sort(data.begin(), data.end());
+	return *this;
+}
+
+
+Foo retVal()
+{
+	Foo ret({453, 345, 3, 123, 56, 73});
+	return ret;
+}
+
+Foo &retFoo()
+{
+	static Foo ret({453, 345, 3, 123, 56, 73});
+	return ret;
+}
+
+std::ostream& print(std::ostream &os, const Foo someFoo)
+{
+	for (const auto &elem : someFoo)
+		os << elem << ' ';
+	return os;
+}
+
+int main()
+{
+	print(std::cout, retVal().sorted()) << '\n';
+	print(std::cout, retFoo().sorted()) << '\n';
+	return 0;
+}
