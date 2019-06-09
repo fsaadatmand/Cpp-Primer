d8383125cf92125d4aae6a60b18a6ab743c5ec4f
cdude <cdude996@gmail.com>
2019-06-08 22:23:39 -0400

    minor tweaks

--- chapter14/14.43.cpp
+++ chapter14/14.43.cpp
@@ -13,9 +13,9 @@
 int main()
 {
 	using std::placeholders::_1;
+	std::vector<int> ivec{11, 20, 52, 11, 21};
 	int number = 20;           // int to check against container
 	auto hasRemainder = std::bind(std::modulus<int>(), number, _1);
-	std::vector<int> ivec{11, 20, 52, 11, 21};
 	auto result = find_if_not(ivec.cbegin(), ivec.cend(), hasRemainder);
 	std::cout << ((result != ivec.cend()) ? "true" : "false") << '\n';
 	return 0;
