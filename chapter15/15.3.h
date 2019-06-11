840f069f327da908a14c5027fc1398707d8b07a2
cdude <cdude996@gmail.com>
2019-06-11 16:37:18 -0400

    initial commit

---
+++ chapter15/15.3.h
@@ -0,0 +1,35 @@
+/*
+ * Exercise 15.3: Define your own versions of the Quote class and the
+ * print_total function.
+ *
+ * By Faisal Saadatmand
+ */
+
+#include <iostream>
+
+#ifndef QUOTE_H
+#define QUOTE_H
+
+class Quote {
+	friend double print_total(std::ostream &, const Quote &, std::size_t);
+	public:
+		Quote() = default;
+		Quote(const std::string &book, double sales_price) :
+			bookNo(book), price(sales_price) {}
+		std::string isbn() const { return bookNo; }
+		virtual double net_price(std::size_t n) const { return n * price; }
+		virtual ~Quote() = default;
+	private:
+		std::string bookNo;
+	protected:
+		double price = 0.0;
+};
+
+double print_total(std::ostream &os, const Quote &item, std::size_t n)
+{
+	double ret = item.net_price(n);
+	os << "ISBN: " << item.isbn() 
+	   << " # sold: " << n << " total due: " << ret << std::endl;
+	return ret;
+}
+#endif
