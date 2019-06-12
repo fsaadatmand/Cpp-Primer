42f59cd7877ae8eff1ac9f15f2e473c2cb5b3df0
cdude <cdude996@gmail.com>
2019-06-11 22:19:03 -0400

    fix indentation

--- chapter07/Sales_data.h
+++ chapter07/Sales_data.h
@@ -9,20 +9,20 @@ class Sales_data {
 	friend std::ostream& print(std::ostream &, const Sales_data &);
 	friend Sales_data add(Sales_data &, Sales_data &);
 	public:
-	// constructors
-	Sales_data() = default;
-	Sales_data(const std::string &s) : bookNo(s) { }
-	Sales_data(const std::string &s, unsigned n, double p) :
-		       bookNo(s), units_sold(n), revenue(p * n) { } 
-	explicit Sales_data(std::istream &is) { read(is, *this); }
-	// members
-	std::string isbn() const { return bookNo;}
-	Sales_data& combine(const Sales_data &);
+		// constructors
+		Sales_data() = default;
+		Sales_data(const std::string &s) : bookNo(s) { }
+		Sales_data(const std::string &s, unsigned n, double p) :
+			bookNo(s), units_sold(n), revenue(p * n) { } 
+		explicit Sales_data(std::istream &is) { read(is, *this); }
+		// members
+		std::string isbn() const { return bookNo;}
+		Sales_data& combine(const Sales_data &);
 	private:
-	double avg_price() const;
-	std::string bookNo;
-	unsigned units_sold = 0;
-	double revenue = 0.0;
+		double avg_price() const;
+		std::string bookNo;
+		unsigned units_sold = 0;
+		double revenue = 0.0;
 };
 
 // member function's definitions
@@ -45,5 +45,4 @@ inline double Sales_data::avg_price() const
 std::istream& read(std::istream &, Sales_data &);
 std::ostream& print(std::ostream &, const Sales_data &);
 Sales_data add(Sales_data &, Sales_data &);
-
 #endif
