6c254d331e44900e29f5b861f982d935e89397a1
cdude <cdude996@gmail.com>
2019-06-15 17:39:43 -0400

    add comment and a data line

--- chapter15/15.29.cpp
+++ chapter15/15.29.cpp
@@ -43,12 +43,14 @@ int main()
 	basket.push_back(std::make_shared<Bulk_quote>("1-111-11111-1", 15.99, 1, 0.15));
 	basket.push_back(std::make_shared<Bulk_quote>("3-333-33333-3", 19.99, 4, 0.25));
 	basket.push_back(std::make_shared<Bulk_quote>("2-222-22222-2", 23.45, 2, 0.5));
+	basket.push_back(std::make_shared<Bulk_quote>("9-999-99999-9", 23.45, 2, 0.5));
 
 	sort(basket.begin(), basket.end(),
 		[](const std::shared_ptr<Quote> &lhs, const std::shared_ptr<Quote> &rhs)
 		{ return lhs->isbn() < rhs->isbn(); });
 	std::size_t n = 0, total = 0;
 	for (auto it = basket.cbegin(); it != basket.cend(); it += n) {
+		// cout_if will return 1 if the predicate failed
 		n = count_if(basket.cbegin(), basket.cend(),
 				[&](const std::shared_ptr<Quote> &item)
 				{ return item->isbn() == (*it)->isbn(); }); 
