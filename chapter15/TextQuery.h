9e7432a1dbdfc436913b3bb4b1dd22420294195c
cdude <cdude996@gmail.com>
2019-06-16 00:40:10 -0400

    simplify code
    use return lines iterator instead of implementing one with
    QueryResultPtr. Add >> operator.

--- chapter15/TextQuery.h
+++ chapter15/TextQuery.h
@@ -10,7 +10,6 @@
 #include <vector>
 
 class QueryResult;
-class QueryResultPtr;
 class TextQuery {
 	public:
 		using line_no = std::vector<std::string>::size_type;
@@ -39,16 +38,14 @@ TextQuery::TextQuery(std::ifstream &is) : file(new std::vector<std::string>)
 }
 
 class QueryResult {
-	friend class QueryResultPtr;
-	friend std::ostream& print (std::ostream&, const QueryResult&);
+	friend std::ostream& operator<<(std::ostream &, const QueryResult &);
 	public:
 		QueryResult(std::string s,
 			        std::shared_ptr<std::set<TextQuery::line_no>> p,
 					std::shared_ptr<std::vector<std::string>> f) :
 			        sought(s), lines(p), file(f) {}
-		QueryResultPtr begin() const;
-		QueryResultPtr end() const;
-		QueryResultPtr shared_ptr() const;
+		auto begin() const { return lines->begin(); }
+		auto end() const { return lines->end(); }
 		std::shared_ptr<std::vector<std::string>>
 			get_file() const { return file; }
 	private:
@@ -67,95 +64,13 @@ TextQuery::query(const std::string &sought) const
 	return QueryResult(sought, loc->second, file);
 }
 
-class QueryResultPtr {
-	public:
-		QueryResultPtr() : curr(0) {}
-		QueryResultPtr(const QueryResult &a, size_t sz = 0) :
-			wptr(a.lines), curr(sz) {}
-		const TextQuery::line_no& deref() const;
-		QueryResultPtr& operator++();
-		QueryResultPtr& operator--();
-		QueryResultPtr operator++(int);
-		QueryResultPtr operator--(int);
-		const TextQuery::line_no& operator*() const;
-		const TextQuery::line_no* operator->() const;
-	private:
-		std::shared_ptr<std::set<TextQuery::line_no>>
-			check(std::size_t, const std::string &msg) const;
-		std::weak_ptr<std::set<TextQuery::line_no>> wptr;
-		size_t curr;
-};
-
-std::shared_ptr<std::set<TextQuery::line_no>>
-QueryResultPtr::check(std::size_t i, const std::string &msg) const
-{
-	auto ret = wptr.lock();
-	if (!ret)
-		throw std::runtime_error("unbound QueryResultPtr");
-	if (i >= ret->size())
-		throw std::out_of_range(msg);
-	return ret;
-}
-
-QueryResultPtr&
-QueryResultPtr::operator++()
-{
-	check(curr, "increment past end of QueryResultPtr");
-	++curr;
-	return *this;
-}
-
-QueryResultPtr&
-QueryResultPtr::operator--()
-{
-	--curr;
-	check(curr, "decrement past end of QueryResultPtr");
-	return *this;
-}
-
-QueryResultPtr
-QueryResultPtr::operator++(int)
-{
-	auto ret = *this;
-	++*this;
-	return ret;
-}
-
-QueryResultPtr
-QueryResultPtr::operator--(int)
-{
-	auto ret = *this;
-	--*this;
-	return ret;
-}
-
-const TextQuery::line_no&
-QueryResultPtr::operator*() const
-{
-	auto p = check(curr, "dereference past end");
-
-	auto iter = p->begin();
-	for (TextQuery::line_no i = 0; i != curr; ++i)
-		++iter;
-	return *iter;
-}
-
-const TextQuery::line_no*
-QueryResultPtr::operator->() const
-{
-	return & this->operator*();
-}
-
-QueryResultPtr QueryResult::begin() const { return QueryResultPtr(*this); }
-QueryResultPtr QueryResult::end() const { return QueryResultPtr(*this); }
-
 std::string make_plural(std::size_t ctr, const std::string &word,
 		                             const std::string &ending)
 {
 	return (ctr > 1) ? word + ending : word;
 }
 
-std::ostream &print(std::ostream &os, const QueryResult &qr)
+std::ostream& operator<<(std::ostream &os, const QueryResult &qr)
 {
 	os << qr.sought << " occurs " << qr.lines->size() << " "
 	   << make_plural(qr.lines->size(), "time", "s") << std::endl;
@@ -174,7 +89,7 @@ void runQueries(std::ifstream &infile)
 		std::string s;
 		if (!(std::cin >> s) || s == "q")
 			break;
-		print(std::cout, tq.query(s)) << std::endl;
+	std::cout << tq.query(s) << std::endl;
 	}
 }
 #endif
