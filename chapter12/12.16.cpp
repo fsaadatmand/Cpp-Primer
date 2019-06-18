3ac2549aec13ed28c344da11ab149f959bed99e1
cdude <cdude996@gmail.com>
2019-06-18 10:21:52 -0400

    fix typo

--- chapter12/12.16.cpp
+++ chapter12/12.16.cpp
@@ -1,5 +1,5 @@
 /*
- * Exercise 12.16: Compilers don’t always give easy-to-understand error
+ * Exercise 12.16: Compilers don't always give easy-to-understand error
  * messages if we attempt to copy or assign a unique_ptr. Write a program that
  * contains these errors to see how your compiler diagnoses them.
  *
