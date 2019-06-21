bfad0b4a88dc3ff072dfc917f221c13f007c12ec
cdude <cdude996@gmail.com>
2019-06-21 13:53:17 -0400

    fix typo

--- chapter16/16.20.h
+++ chapter16/16.20.h
@@ -1,6 +1,6 @@
 /*
  * Exercise 16.20: Rewrite the function from the previous exercise to use
- * iterators re- turned from begin and end to control the loop.
+ * iterators returned from begin and end to control the loop.
  *
  * By Faisal Saadatmand
  */
