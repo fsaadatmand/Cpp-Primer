3d8b5414502030fe6d8630d5a6470424f3a6f1b0
cdude <cdude996@gmail.com>
2019-07-10 15:37:46 -0400

    initial commit

---
+++ chapter16/16.39.cpp
@@ -0,0 +1,26 @@
+/*
+ * Exercise 16.39: Use an explicit template argument to make it sensible to
+ * pass two string literals to the original version of compare from § 16.1.1
+ * (p. 652).
+ *
+ * By Faisal Saadatmand
+ */
+
+#include <iostream>
+#include <string>
+
+
+template <typename T>
+int compare(const T &v1, const T &v2)
+{
+	if (v1 < v2) return -1;
+	if (v2 < v1) return 1;
+	return 0;
+}
+
+int main() {
+
+	std::cout << compare<std::string>("world", "world") << '\n';
+
+	return 0;
+}
