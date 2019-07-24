4c42994a9377436aee803dfcd5cf87a7819bbe48
cdude <cdude996@gmail.com>
2019-07-24 11:36:49 -0400

    initial commit

---
+++ chapter18/18.3.cpp
@@ -0,0 +1,43 @@
+/*
+ * Exercise 18.3: There are two ways to make the previous code work correctly
+ * if an exception is thrown. Describe them and implement them.
+ *
+ * By Faisal Saadatmand
+ */
+
+/*
+ * (1) Use a smart pointer instead of a built-in pointer and new expression
+ * (2) Allocate the array on the stack instead of the heap.
+ */
+
+#include <iostream>
+#include <exception>
+#include <memory>
+#include <vector>
+
+void exercise_samrtPtr(int *b, int *e)
+{
+	std::vector<int> v(b, e);
+	std::unique_ptr<int[]> up(new int[v.size()]);
+	throw std::runtime_error("smartPtr error");;
+}
+
+void exercise_stack(int *b, int *e)
+{
+	std::vector<int> v(b, e);
+	int a[v.size()];
+	throw std::runtime_error("Stack error");;
+}
+
+int main()
+{
+	int a[10] = {0 , 1, 2, 3, 4, 5, 6, 7, 8, 9};
+	try {
+	exercise_stack(a, &a[10]);
+	exercise_samrtPtr(a, &a[10]);
+	}
+	catch (std::runtime_error) {
+		std::cerr << "caught exception\n";
+	}
+	return 0;
+}
