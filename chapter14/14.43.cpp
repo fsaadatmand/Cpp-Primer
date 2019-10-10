7fa5d575afb8ae59d204d3f853f4547889240b60
cdude <cdude996@gmail.com>
2019-10-10 11:03:34 -0400

    add solution using lambda

--- chapter14/14.43.cpp
+++ chapter14/14.43.cpp
@@ -12,11 +12,20 @@
 
 int main()
 {
-	using std::placeholders::_1;
 	std::vector<int> ivec{11, 20, 52, 11, 21};
-	int number = 20;           // int to check against container
+	const int number = 20;
+
+	// Using lambda
+	auto mod = std::modulus<int>();
+	auto result1 = std::any_of(ivec.cbegin(), ivec.cend(),
+			[&](const int &elem) { return !mod(number, elem); });
+	std::cout << (result1 ? "true" : "false") << '\n';
+
+	// Using bind
+	using std::placeholders::_1;
 	auto hasRemainder = std::bind(std::modulus<int>(), number, _1);
-	auto result = find_if_not(ivec.cbegin(), ivec.cend(), hasRemainder);
-	std::cout << ((result != ivec.cend()) ? "true" : "false") << '\n';
+	auto result2 = find_if_not(ivec.cbegin(), ivec.cend(), hasRemainder);
+	std::cout << ((result2 != ivec.cend()) ? "true" : "false") << '\n';
+
 	return 0;
 }
