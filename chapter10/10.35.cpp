6e47f953cf7639ce2b22f7a2d5fa46bd75037d27
cdude <cdude996@gmail.com>
2019-10-17 22:12:59 -0400

    minor tweaks

--- chapter10/10.35.cpp
+++ chapter10/10.35.cpp
@@ -12,7 +12,7 @@ int main()
 {
 	std::vector<int> ivec{0, 1, 2, 3, 4, 5, 6, 7, 8 , 9};
 	for(auto it = ivec.cend() - 1; it >= ivec.begin(); --it)
-		std::cout << *it << ' ';
-	std::cout << std::endl;
+		std::cout << ' ' << *it;
+	std::cout << '\n';
 	return 0;
 }
