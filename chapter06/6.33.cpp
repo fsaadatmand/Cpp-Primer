55ee3de8144ff09d58a141b07f921b06035aa5ae
cdude <cdude996@gmail.com>
2019-11-17 19:19:07 -0500

    revise answer

--- chapter06/6.33.cpp
+++ chapter06/6.33.cpp
@@ -7,27 +7,18 @@
 #include <iostream>
 #include <vector>
 
-void printVec(std::vector<int>::iterator, std::vector<int>::iterator);
-
-void printVec(std::vector<int>::iterator beg, std::vector<int>::iterator end)
+void printVec(std::vector<int>::const_iterator beg,
+				std::vector<int>::const_iterator end)
 {
 	if (beg == end)
 		return;
-	std::cout << *beg << " ";
+	std::cout << " " << *beg;
 	printVec(beg + 1, end);
 }
 
 int main() {
 	std::vector<int> v1{50, 23, 63, 99, 32, 74};
-
-	// iterative
-	for (auto it = v1.begin(); it != v1.end(); ++it)
-		std::cout << *it << " ";
+	printVec(v1.cbegin(), v1.cend());
 	std::cout << '\n';
-
-	// recursive
-	printVec(v1.begin(), v1.end());
-	std::cout << '\n';
-
 	return 0;
 }
