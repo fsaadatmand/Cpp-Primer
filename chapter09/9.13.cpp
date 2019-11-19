d84cd47a8461a4e8cdb7b45c4da130e052a9acb9
cdude <cdude996@gmail.com>
2019-11-18 19:54:00 -0500

    fix format

--- chapter09/9.13.cpp
+++ chapter09/9.13.cpp
@@ -1,11 +1,13 @@
 /*
  * Exercise 9.13: How would you initialize a vector<double> from a list<int>?
  *
- * Answer: by using the constructor that takes input iterators. Note that an
- * implicit conversion of each each element (from int to double) will take
- * place during the initialization.
- *
  * By Faisal Saadatmand
+ */
+
+/*
+ * Answer: by using the constructor that takes input iterators. Note that an
+ * implicit conversion of each element (from int to double) will take place
+ * during the initialization.
  */
 
 #include <iostream>
@@ -18,7 +20,7 @@ int main()
 	std::vector<double> v1(l1.cbegin(), l1.cend());
 
 	for (const auto &elem : v1)
-		std::cout << " " << elem;
+		std::cout << ' ' << elem;
 	std::cout << '\n';
 	return 0;
 }
