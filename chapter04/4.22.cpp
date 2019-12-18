7a04710c4218ea93b142f4c1e68b556b961382cc
cdude <cdude996@gmail.com>
2019-12-17 23:42:49 -0500

    split answers into two files
    In addition to simplifying the code, I split the two approaches into two
    files: conditional operators version in 4.22.cpp and if statements
    version in 4.22a.cpp

--- chapter04/4.22.cpp
+++ chapter04/4.22.cpp
@@ -5,35 +5,28 @@
  * should use one or more if statements. Which version do you think is easier
  * to understand and why?
  *
- * Answer: about the same, provided that you are comfortable with both
- * statements. That being said, anything beyond 3 nested conditional operators
- * can become quite illegible.
- *
  * By Faisal Saadatmand
  */
 
+/*
+ * Answer: about the same, provided that you are comfortable with both
+ * approaches. That being said, anything beyond 3 nested conditional operators
+ * could become quite illegible.
+ */
+ 
+// conditional operators version. See 4.22a.cpp for the if statements version.
+//
 #include <iostream>
 #include <string>
-#include <vector>
 
 int main()
 {
 	int grade;
 	std::cin >> grade;
-//	std::string finalgrade = (grade > 90) ? "high pass"
-//		                   : (grade >= 65 && grade <= 75) ? "low pass"
-//						   : (grade < 60) ? "fail" : "pass";
-
-	std::string finalgrade = (grade > 90) ? "high pas" : (grade < 60) ? "fail" : "pass";
+	std::string finalgrade = (grade > 90) ? "high pass"
+		                   : (grade > 75) ? "pass"
+						   : (grade >= 60 && grade <= 75) ? "low pass"
+						   : "fail";
 	std::cout << finalgrade << '\n';
-
-	if (grade > 90)
-		std::cout << "high pass\n";
-	else if (grade >= 65 && grade<= 75)
-		std::cout << "low pass\n";
-	else
-		std::cout << ((grade < 60) ? "fail\n" : "pass\n");
-
-
 	return 0;
 }
