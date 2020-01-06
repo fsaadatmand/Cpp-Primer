35da9b19be568a91c07c6c6ad98296031126fa44
cdude <cdude996@gmail.com>
2020-01-06 17:53:02 -0500

    clarify comments

--- chapter02/2.08.cpp
+++ chapter02/2.08.cpp
@@ -10,9 +10,12 @@
 
 int main()
 {
+	// 2M followed by a newline
 	std::cout << "\062\115" << std::endl; // "2M" octal decimals
-	std::cout << "\062\t\115\n";          // octal decimals and escape characters
-	std::cout << "\062\011\115\012";      // octal decimals
-	std::cout << "\x32\x09\x4d\x0a";      // hexadecimals
+	// 2, then a tab, then an M, followed by a newline
+	// any of followings would work:
+	std::cout << "\062\t\115\n"; // octal decimals and escape characters
+	std::cout << "\062\011\115\012"; // octal decimals
+	std::cout << "\x32\x09\x4d\x0a"; // hexadecimals
 	return 0;
 }
