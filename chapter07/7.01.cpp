5dc8868c31745c918346cf3d4b5cf6c809973088
cdude <cdude996@gmail.com>
2020-01-24 14:53:32 -0500

    simplify istream conditions

--- chapter07/7.01.cpp
+++ chapter07/7.01.cpp
@@ -19,30 +19,27 @@ int main()
 	Sales_data total;
 	double averagePrice = 0.0;
 	double price = 0.0;
-
-	if (std::cin >> total.bookNo && std::cin
-			     >> total.units_sold && std::cin >> price) {
+	if (std::cin >> total.bookNo >> total.units_sold >> price) {
 		total.revenue = total.units_sold * price;
 		Sales_data trans;
-			while (std::cin >> trans.bookNo && std::cin
-						 >> trans.units_sold && std::cin >> price) {
-				trans.revenue = trans.units_sold * price;
-				if (total.bookNo == trans.bookNo) {
-					total.units_sold += trans.units_sold;
-					total.revenue += trans.revenue;
-					averagePrice = total.revenue / total.units_sold;
-				} else {
-					std::cout << total.bookNo << " "
-						      << total.units_sold << " "
-							  << total.revenue << " "
-							  << averagePrice << std::endl;
-					total = trans;
-				}
+		while (std::cin >> trans.bookNo >> trans.units_sold >> price) {
+			trans.revenue = trans.units_sold * price;
+			if (total.bookNo == trans.bookNo) {
+				total.units_sold += trans.units_sold;
+				total.revenue += trans.revenue;
+				averagePrice = total.revenue / total.units_sold;
+			} else {
+				std::cout << total.bookNo << " "
+					<< total.units_sold << " "
+					<< total.revenue << " "
+					<< averagePrice << std::endl;
+				total = trans;
 			}
-			std::cout << total.bookNo << " "
-				<< total.units_sold << " "
-				<< total.revenue << " "
-				<< averagePrice << std::endl;
+		}
+		std::cout << total.bookNo << " "
+			<< total.units_sold << " "
+			<< total.revenue << " "
+			<< averagePrice << std::endl;
 	} else {
 		std::cerr << "No data?!" << std::endl;
 		return -1;
