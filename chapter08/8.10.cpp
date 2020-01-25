808c7477a5ce8d64de859a78a2c8a9aa60248f5e
cdude <cdude996@gmail.com>
2020-01-24 22:22:55 -0500

    simplify code
    Used print function to simplify main, plus other minor tweaks.

--- chapter08/8.10.cpp
+++ chapter08/8.10.cpp
@@ -1,6 +1,6 @@
 /*
  * Exercise 8.10: Write a program to store each line from a file in a
- * vector<string>.  Now use an istringstream to read each element from the
+ * vector<string>. Now use an istringstream to read each element from the
  * vector a word at a time.
  *
  * By Faisal Saadatmand
@@ -12,40 +12,33 @@
 #include <string>
 #include <vector>
 
-std::istream &printWords(std::istream &);
-
-std::istream &printWords(std::istream &is)
+std::vector<std::string> loadFile(const std::string &file)
 {
-	std::string word;
-	while (is >> word)
-		std::cout << word << '\n';
-	is.clear();
-	return is;
+	std::ifstream input(file);
+	if (!input) {
+		std::cerr << "counldn't open: " + file << '\n';
+		exit(EXIT_FAILURE); // C-style exit
+	}
+	std::vector<std::string> data;
+	std::string line;
+	while (getline(input, line))
+		data.push_back(line);
+	return data;
 }
 
 int main(int argc, char **argv)
 {
-	if (argc != 2) {
-		std::cerr << "Usage: " + std::string(*argv) + " file\n";
+	if (--argc != 1) {
+		std::cerr << "Usage: " + std::string(*argv) + " <filename>\n";
 		return -1;
 	}
-
 	auto p = argv + 1;
-	std::ifstream infile(*p);
-	if (!infile) {
-		std::cerr << "Couldn't open " + std::string(*p) << '\n';
-		return -1;
-	}
-
-	std::string line;
-	std::vector<std::string> content;
-	while (infile >> line)
-		content.push_back(line);
-
-	std::string word;
-	for (const auto &element : content) {
-		std::istringstream word(element);
-		printWords(word);
+	std::vector<std::string> file = loadFile(*p);
+	for (const auto &line : file) {
+		std::istringstream ss(line);
+		std::string word;
+		while (ss >> word)
+			std::cout << word << '\n';
 	}
 	return 0;
 }
--- chapter08/8.11.cpp
+++ chapter08/8.11.cpp
@@ -4,12 +4,15 @@
  * were defined outside that loop? Rewrite the program, moving the definition
  * of record outside the 
  * 
+ *
+ * By Faisal Saadatmand
+ */
+
+/*
  * Answer: first, declare the unbound stream record outside the outer loop,
  * than copy the string line into the stream at every iteration of the otter
  * loop, and, finally, clear the stream just before looping back so it will be
  * in a valid state on the next line copy.
- *
- * By Faisal Saadatmand
  */
 
 #include <iostream>
@@ -22,28 +25,29 @@ struct PersonInfo {
 	std::vector<std::string> phones;
 };
 
+std::ostream& print(std::ostream &os, const PersonInfo &person)
+{
+	os << person.name;
+	for (const auto &number : person.phones)
+		std::cout << ' ' << number;
+	return os;
+}
+
 int main()
 {
 	std::string line, word;
 	std::vector<PersonInfo> people;
-	std::istringstream record;     // unbound istringstream
-
+	std::istringstream record; // unbound istringstream
 	while (getline(std::cin, line)) {
 		PersonInfo info;
-		record.str(line);          // copy line to stream
+		record.str(line); // copy line to stream
 		record >> info.name;
 		while (record >> word)
 			info.phones.push_back(word);
 		people.push_back(info);
-		record.clear();            // rest stream back to valid state
+		record.clear(); // rest stream back to valid state
 	}
-
-	// print vector
-	for (const auto &person : people) {
-		std::cout << person.name << " ";
-		for (const auto &number : person.phones)
-			std::cout << number << " ";
-		std::cout << '\n';
-	}
+	for (const auto &person : people) // print vector
+		print(std::cout, person) << '\n';
 	return 0;
 }
